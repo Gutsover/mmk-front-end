25d284bea83009c9fdffd5d7e807752006605ee6
Mylene <mylenevandaele@gmail.com>
2021-12-11 22:18:37 +0100

    create views folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeHeaderComponent } from './header/home-header.component';
 import { FormSigninHomeComponent } from './form-signin-home/form-signin-home.component';
+import { SidebarComponent } from './view/sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeHeaderComponent,
-    FormSigninHomeComponent
+    FormSigninHomeComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
