ccd6e642e17c4cc63bf87b2edc9c8c0700c6fad5
Kevin Moulun <Moulun.kevinn@gmail.com>
2021-12-21 03:09:06 +0100

    Add routing and dashboard component

--- src/app/header/home-header.component.ts
+++ src/app/header/home-header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeHeaderComponent implements OnInit {
 
+  user = null;
   today = new Date().toLocaleDateString();
   time!: string;
   
