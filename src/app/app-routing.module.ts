7a5a4bff4746c9a2ca2011fa5ce5c3979039d7c6
Atndesign <Atnnetworkk@gmail.com>
2021-12-28 17:41:55 +0100

    add audit client card component with scss styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormSigninHomeComponent } from './view/form-signin-home/form-signin-home.component';
-import { DashboardComponent } from './view/dashboard/dashboard.component';
-import { ClientViewComponent } from './view/client-view/client-view.component';
-import { AdvisorsListComponent } from './view/advisors-list/advisors-list.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormSigninHomeComponent} from './view/form-signin-home/form-signin-home.component';
+import {DashboardComponent} from './view/dashboard/dashboard.component';
+import {ClientViewComponent} from './view/client-view/client-view.component';
+import {AdvisorsListComponent} from './view/advisors-list/advisors-list.component';
+import {AuditComponent} from './view/audit/audit.component';
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: FormSigninHomeComponent },
-  { path: 'client', component: ClientViewComponent },
-  { path: 'advisors', component: AdvisorsListComponent },
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: FormSigninHomeComponent},
+  {path: 'client', component: ClientViewComponent},
+  {path: 'advisors', component: AdvisorsListComponent},
+  {path: 'adm/audit', component: AuditComponent},
 ];
 
 @NgModule({
