4b2e7510153b2818af4edc270351a8f431fa2d97
Atndesign <Atnnetworkk@gmail.com>
2021-12-28 17:20:05 +0100

    add audit container page

---
+++ src/app/components/audit-client-card/audit-client-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuditClientCardComponent } from './audit-client-card.component';
+
+describe('AuditClientCardComponent', () => {
+  let component: AuditClientCardComponent;
+  let fixture: ComponentFixture<AuditClientCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AuditClientCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuditClientCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/audit-client-card/audit-client-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-client-card',
+  templateUrl: './audit-client-card.component.html',
+  styleUrls: ['./audit-client-card.component.scss']
+})
+export class AuditClientCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
