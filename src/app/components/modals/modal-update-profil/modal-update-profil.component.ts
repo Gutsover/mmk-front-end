66831391f999744a18d43b20ae005de57a993fdb
Mylene <mylenevandaele@gmail.com>
2022-01-16 13:26:17 +0100

    add modalfor update user profile

---
+++ src/app/components/modals/modal-update-profil/modal-update-profil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-update-profil',
+  templateUrl: './modal-update-profil.component.html',
+  styleUrls: ['./modal-update-profil.component.scss']
+})
+export class ModalUpdateProfilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
