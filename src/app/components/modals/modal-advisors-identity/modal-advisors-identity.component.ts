2acca774249cfb4ac2a072c85004180d656a0535
Mylene <mylenevandaele@gmail.com>
2022-01-20 18:57:28 +0100

    add error formGroup

--- src/app/components/modals/modal-advisors-identity/modal-advisors-identity.component.ts
+++ src/app/components/modals/modal-advisors-identity/modal-advisors-identity.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-modal-client-identity',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-advisors-identity.component.scss'],
 })
 export class ModalAdvisorsIdentity implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {}
+  advisorUpdateForm : any;
+
+  
+
+  ngOnInit(): void {
+    this.advisorUpdateForm = this.fb.group({
+      firstName: ['', Validators.required],
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+    });
+  }
+
+  send() {
+
+  }
 }
+
+
