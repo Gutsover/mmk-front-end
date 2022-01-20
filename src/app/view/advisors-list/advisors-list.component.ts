27ae4908e3d73d32dfad0e350212831fa1299f4e
Mylene <mylenevandaele@gmail.com>
2022-01-20 18:11:07 +0100

    add fetch for listAdvisors and advisor identity

--- src/app/view/advisors-list/advisors-list.component.ts
+++ src/app/view/advisors-list/advisors-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AdvisorsService } from 'src/app/services/advisors.service';
 
 @Component({
   selector: 'app-advisors-list',
@@ -6,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advisors-list.component.scss'],
 })
 export class AdvisorsListComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  @Input()
+  currentAdvisorId: number = 1;
+
+  advisors: any;
+
+  constructor(private advisorService: AdvisorsService) {}
+
+  ngOnInit(): void {
+    this.advisorService.getAdvisors().subscribe((res) => (this.advisors = res));
+  }
+
+  updateAdvisorInfo(id: any): void {
+    this.currentAdvisorId = id;
+  }
+
+
 }
