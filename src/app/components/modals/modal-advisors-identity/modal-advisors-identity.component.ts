4819338d5ab790d299bd45c50dabb69c99555ad4
Mylene <mylenevandaele@gmail.com>
2022-01-20 20:34:15 +0100

    add current Advisor on fields form modal update

--- src/app/components/modals/modal-advisors-identity/modal-advisors-identity.component.ts
+++ src/app/components/modals/modal-advisors-identity/modal-advisors-identity.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 
 @Component({
@@ -9,16 +10,18 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 })
 export class ModalAdvisorsIdentity implements OnInit {
 
-  constructor(private fb: FormBuilder) {}
+  constructor(public dialogRef: MatDialogRef<ModalAdvisorsIdentity>,
+    @Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder) {}
   advisorUpdateForm : any;
-
+    currentAdvisor: any; 
   
 
   ngOnInit(): void {
+    this.currentAdvisor = this.data.currentAdvisor;
     this.advisorUpdateForm = this.fb.group({
-      firstName: ['', Validators.required],
-      name: ['', Validators.required],
-      email: ['', Validators.required],
+      firstName: [this.data.currentAdvisor.firstName, Validators.required],
+      name: [this.data.currentAdvisor.name, Validators.required],
+      email: [this.data.currentAdvisor.email, Validators.required],
     });
   }
 
