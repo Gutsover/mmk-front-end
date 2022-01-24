5c4d622efcafc04de690742d91ef39d70b91d20b
Mylene <mylenevandaele@gmail.com>
2022-01-24 19:49:51 +0100

    add pipe custom phoneFormat

--- src/app/pipes/phone-format.pipe.ts
+++ src/app/pipes/phone-format.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PhoneFormatPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any) {
+    value = value.charAt(0) != 0 ? "0" + value : "" + value;
+
+    let newValue = "";
+    let i = 0;
+
+    for (;i < Math.floor(value.length / 2) - 1; i++) {
+      newValue = newValue + value.substr(i*2, 2) + ".";
+    }
+
+    return newValue + value.substr(i*2);
   }
 
 }
