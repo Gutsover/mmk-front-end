61642746e2e9c1926866805747712578e007768e
Mylene <mylenevandaele@gmail.com>
2022-01-25 16:04:40 +0100

    add view and component hystory transactions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -62,6 +62,11 @@ const routes: Routes = [
     canActivate: [AuthGuardService, OwnerGuardService],
   },
   {
+    path: 'adm/transactions',
+    component: CreateAdvisorComponent,
+    canActivate: [AuthGuardService, OwnerGuardService],
+  },
+  {
     path: '**',
     pathMatch: 'full',
     component: ErrorpageComponent,
---
+++ src/app/view/admin/history-transactions/history-transactions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history-transactions',
+  templateUrl: './history-transactions.component.html',
+  styleUrls: ['./history-transactions.component.scss']
+})
+export class HistoryTransactionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
